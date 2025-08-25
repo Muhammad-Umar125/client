2dea5872bdf75d4e600a1e88ccb3798084b1c8d6
Muhammad-Umar125 <umerq0875@gmail.com>
2025-08-25 18:40:07 +0500

    created recent messages component

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { dummyPostsData } from '../assets/assets'
+import { assets, dummyPostsData } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
 import PostCard from '../components/PostCard'
+import RecentMessages from '../components/RecentMessages'
 
 
 const Feed = () => {
@@ -25,21 +26,23 @@ const Feed = () => {
       <div>
         <StoriesBar />
         <div className='p-4 space-y-6'>
-          {feeds.map((post)=>(
-            <PostCard key={post._id} post={post}/>
-          ))} 
+          {feeds.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
         </div>
       </div>
 
       {/* right sidebar */}
-      <div>
-        <div>
-          <h1>Sponsored</h1>
+      <div className='max-xl:hidden '>
+        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
+          <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
+          <img src={assets.sponsored_img} className='w-75 h-50 roundedmd ' alt="" />
+          <p className='text-slate-600'>Email marketing</p>
+          <p className='text-slate-400'>Supercharge your marketing with a powerful, easy-to-use platform built for results.</p>
         </div>
-        <h1>Recent messages</h1>
+        <RecentMessages />
+
       </div>
-
-
     </div>
   ) : <Loading />
 }
