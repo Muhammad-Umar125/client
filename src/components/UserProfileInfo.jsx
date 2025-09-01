b6226f41e748f17451d672a11d137bbf346b3ec3
Muhammad-Umar125 <umerq0875@gmail.com>
2025-09-01 20:12:01 +0500

    created Profile page and added user profile component

---
+++ src/components/UserProfileInfo.jsx
@@ -0,0 +1,46 @@
+import { Calendar, MapPin, PenBox, Verified } from 'lucide-react'
+import moment from 'moment'
+import React from 'react'
+
+const UserProfileInfo = ({ user, posts, profileId, setShowEdit }) => {
+  return (
+    <div className='relative p-4 px-6 md:px-8 bg-white'>
+      <div className=' flex flex-col md:flex-row items-start gap-6'>
+        <div className='w-32 h-32  border-4 border-white -top-16 shadow-lg rounded-full absolute'>
+          <img src={user.profile_picture} className='absolute rounded-full  z-2' alt="" />
+        </div>
+        <div className='w-full pt-16 md:pt-0 md:pl-36  border border-red-600  '>
+          <div className=' flex flex-col md:flex-row items-start justify-between'>
+            <div>
+              <div className='flex  font-extrabold text-xl items-center gap-3'>
+                <h1 className='text-2xl font-bold text-gray-900'>{user.full_name}</h1>
+                <Verified className='w-6 h-6 text-blue-500' />
+              </div>
+              <p className='text-gray-600'>{user.username ? `@${user.username}` : 'Add a username'}</p>
+            </div>
+            {/* if user it not on other profile so we will give it the edit option */}
+            {!profileId &&
+              <button onClick={() => setShowEdit(true)} className='flex items-center gap-2 border border-gray-300 hover:bg-gray-50 px-4 py-2 rounded-lg font-medium transition-colors mt-4 md:mt-0 cursor-pointer'>
+                <PenBox className='w-4 h-4' />
+                Edit
+              </button>}
+          </div>
+          <p className='text-gray-700 text-sm max-w-md mt-4'>{user.bio}</p>
+          <div className='flex flex-wrap items-center gap-x-6 gap-y-2 text-sm text-gray-500 mt-4'>
+            <span className='flex items-center gap-1.5'>
+              <MapPin className='w-4 h-4' />
+              {user.location ? user.location : 'Add location'}
+            </span>
+            <span className='flex items-center gap-1.5'>
+              <Calendar className='w-4 h-4' />
+              Joined <span className='font-medium'>{moment(user.createdAt).fromNow()}</span>
+            </span>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default UserProfileInfo
